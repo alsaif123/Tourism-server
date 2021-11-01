483c7bf3e785a70b2572b3554416b259938646e9
alsaif123 <alsaif200200@gmail.com>
2021-11-01 22:20:31 +0600

    deployment stage

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const colors = require("colors");
 const cors = require("cors");
 const app = express();
-const { MongoClient } = require('mongodb');
-const port=5000;
+const { MongoClient } = require("mongodb");
+const port = 5000;
 app.use(express.json());
 app.use(cors());
 // root app
@@ -10,17 +11,37 @@ app.get("/", (req, res) => {
   res.send("Welcome to server side application");
 });
 // env file baki ace
-const uri = "mongodb+srv://tourDB:tourdb@cluster0.iozoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("Database is ready for tourism application");
- 
+const uri =
+  "mongodb+srv://tourDB:tourdb@cluster0.iozoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+// add places
+client.connect((err) => {
+ 
+const adminCollection = client.db("tourDB").collection("places");
+console.log(`Error : ${err}`.red);
+console.log(`MongoDb connected for Job task`.magenta);
 
-
+// add blog
+app.post("/addPlaces", (req, res) => {
+  const newPlace= req.body;
+  console.log(newPlace);
+  adminCollection.insertOne(newPlace).then((result) => {
+    console.log(result.insertedCount > 0);
+    res.send(result.insertedCount > 0);
+  });
+});
+// getPlaces and show on home
+app.get("/getPlaces", (req, res) => {
+  adminCollection.find({}).toArray((err, docs) => {
+    res.send(docs);
+  });
+});
+});
 
 // default route
 app.listen(port, () => {
-    console.log("Welcome to server side app");
-  });
+  console.log("Welcome to server side app");
+});
